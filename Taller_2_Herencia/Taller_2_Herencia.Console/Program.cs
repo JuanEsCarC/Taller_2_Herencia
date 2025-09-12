7a0e67f79219efb021eac465da27635ec104c314
Juan Esteban Cárdenas Cardona <juanesteban1417@gmail.com>
2025-09-11 20:55:51 -0500

    Kite class added

--- Taller_2_Herencia/Taller_2_Herencia.Console/Program.cs
+++ Taller_2_Herencia/Taller_2_Herencia.Console/Program.cs
@@ -7,11 +7,12 @@ try
     var circle = new Circle(nameof(Circle), 5);
     var square = new Square(nameof(Square), 10);
     var rhombus = new Rhombus(nameof(Rhombus), 5, 7, 10);
+    var kite = new Kite(nameof(Kite), 7, 6, 5, 8);
     var rectangle = new Rectangle(nameof(Rectangle), 4.568, 67.790);
+  
 
 
-
-    var figures = new List<GeometricFigure>() { circle, square, rhombus, rectangle };
+    var figures = new List<GeometricFigure>() { circle, square, rhombus, kite, rectangle };
     foreach (var figure in figures)
     {
         Console.WriteLine(figure);
---
+++ Taller_2_Herencia/Taller_2_Herencia.Core/Kite.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_2_Herencia.Core;
+
+public class Kite : Rhombus
+{
+
+    //Fields
+    private double _b;
+
+    //Properties
+    public double B
+    {
+        get => _b;
+        set => _b = ValidateB(value);
+    }
+
+
+    //Constructor
+    public Kite(string name, double a, double d1, double d2, double b) : base(name, a, d1, d2)
+    {
+        B = b;
+    }
+
+
+    //Methods
+    public override double GetArea()
+    {
+        return (D1 * D2) / 2;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 2 * (A + B);
+    }
+
+    private double ValidateB(double b)
+    {
+        if (b <= 0)
+        {
+            throw new Exception($"The value {b} must be positive and greater than zero.");
+        }
+        return b;
+    }
+
+
+
+
+
+
+
+
+}
