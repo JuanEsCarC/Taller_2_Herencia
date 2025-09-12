d9b8a05c2a0b6a4cd6dabf1ca30266b2cf780d0a
Juan Esteban Cárdenas Cardona <juanesteban1417@gmail.com>
2025-09-11 21:58:41 -0500

    Exercise completed

---
+++ Taller_2_Herencia/Taller_2_Herencia.Core/Trapeze.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_2_Herencia.Core;
+
+public class Trapeze : Triangle
+{
+    //Fields
+    private double _d;
+
+    //Properties
+    public double D
+    {
+        get => _d;
+        set => _d = ValidateD(value);
+    }
+
+    //Constructor
+    public Trapeze(string name, double a, double b, double c, double h, double d) : base(name, a, b, c, h)
+    {
+        D = d;
+    }
+
+    public override double GetArea()
+    {
+        return ((B + D) * H) / 2;
+    }
+
+    public override double GetPerimeter()
+    {
+        return A + B + C + D;
+    }
+
+    private double ValidateD(double d)
+    {
+        if (d <= 0)
+        {
+            throw new Exception($"The value {d} must be positive and greater than zero.");
+        }
+        return d;
+    }
+
+}
+
